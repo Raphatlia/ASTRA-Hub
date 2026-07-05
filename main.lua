e82d1de3b4f178598a6d7f51ffde0862b1502e33
Raphatlia <mma30k@gmail.com>
2026-07-05 22:38:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- ASTRA HUB V1.0
+-- ASTRA HUB V3.0 — ФИНАЛ (БЫСТРАЯ АНИМАЦИЯ + ФОН)
 local Players = game:GetService("Players")
 local LP = Players.LocalPlayer
 local UserInputService = game:GetService("UserInputService")
@@ -1307,4 +1307,4 @@ pcall(function()
 end)
 
 ShowToast("ASTRA HUB загружен!", true)
-print("ASTRA HUB V1.0)
+print("ASTRA HUB V3.0 — БЫСТРАЯ АНИМАЦИЯ + ФОН ЗАГРУЖЕН!")
